d9045725f2ccf40d8c0b62f491cdcaeb4dc1e18d
Tamilvanan <57147424+tamilvanan31@users.noreply.github.com>
2023-05-20 16:07:24 +0530

    add card

--- utils.js
+++ utils.js
@@ -1,6 +1,9 @@
 function printValue() {
-  document.getElementById("output").value =
-    document.getElementById("input-1").value;
+  document.getElementById("output").value = `[${
+    document.getElementById("input-1").value
+  }, ${document.getElementById("input-2").value}, ${
+    document.getElementById("input-3").value
+  }]`;
 }
 
 function copyToClipBoard() {
